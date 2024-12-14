d3f04c71762d8dd6e42ffe7a0f9e1a15b385b435
Aniket Ingole <aniketingolenet@gmail.com>
2024-12-14 21:41:55 +0530

    Added custom hook for useAuth

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <AppContextProvider>
         <AppRoutes />
-        <Toaster />
       </AppContextProvider>
+      <Toaster />
     </QueryClientProvider>
   );
 }
