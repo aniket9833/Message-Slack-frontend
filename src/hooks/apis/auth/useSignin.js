1dc035cf088e075d976702cb432b38e7917bf7a8
Aniket Ingole <aniketingolenet@gmail.com>
2024-12-15 22:13:42 +0530

    Added protected routes

--- src/hooks/apis/auth/useSignin.js
+++ src/hooks/apis/auth/useSignin.js
@@ -1,10 +1,13 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { signInRequest } from '@/apis/auth';
+import { useAuth } from '@/hooks/context/useAuth';
 import { useToast } from '@/hooks/use-toast';
 
 export const useSignin = () => {
   const { toast } = useToast();
+  const { setAuth } = useAuth();
+
   const {
     isPending,
     isSuccess,
@@ -20,6 +23,12 @@ export const useSignin = () => {
 
       localStorage.setItem('user', userObject);
       localStorage.setItem('token', response.data.token);
+
+      setAuth({
+        token: response.data.token,
+        user: response.data,
+        loading: false,
+      });
 
       toast({
         title: 'Successfully signed in',
