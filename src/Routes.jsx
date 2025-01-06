6ea3a1e20f3d6af122db6cc9891a25bb83f22c89
Aniket Ingole <aniketingolenet@gmail.com>
2025-01-06 21:02:22 +0530

    Added sidebar item

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -46,6 +46,10 @@ export const AppRoutes = () => {
           </ProtectedRoute>
         }
       />
+      <Route
+        path="/workspaces/:workspaceId/channels/:channelId"
+        element={<ProtectedRoute>Channel</ProtectedRoute>}
+      />
       <Route path="/*" element={<Notfound />} />
     </Routes>
   );
