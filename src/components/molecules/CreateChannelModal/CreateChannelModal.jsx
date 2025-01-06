9a30e3f8e74d30ca261e06273b8a8a3e4939bec9
Aniket Ingole <aniketingolenet@gmail.com>
2025-01-06 20:25:03 +0530

    Added create channel modal ui

---
+++ src/components/molecules/CreateChannelModal/CreateChannelModal.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { useCreateChannelModal } from '@/hooks/context/useCreateChannelModal';
+
+export const CreateChannelModal = () => {
+  const { openCreteChannelModal, setOpenCreateChannelModal } =
+    useCreateChannelModal();
+
+  const [channelName, setChannelName] = useState();
+
+  function handleClose() {
+    setOpenCreateChannelModal(false);
+  }
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <Dialog open={openCreteChannelModal} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create new Channel</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleFormSubmit}>
+          <Input
+            value={channelName}
+            onChange={(e) => setChannelName(e.target.value)}
+            minLength={3}
+            placeholder="Channel Name e.g. team-announcements"
+            required
+          />
+          <div className="flex justify-end mt-4">
+            <Button>Create Channel</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
